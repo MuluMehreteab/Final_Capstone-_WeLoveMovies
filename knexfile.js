579b62e53bbba071973e103f3f09dc2b1089190d
MuluMehreteab <mulu.mehreteab@gmail.com>
2024-08-06 00:35:32 -0400

    updated modified knexfile

--- knexfile.js
+++ knexfile.js
@@ -5,13 +5,12 @@ require("dotenv").config();
 const {
   NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL, 
+  PRODUCTION_DATABASE_URL,
 } = process.env;
-
 const URL =
-NODE_ENV === "production"
-? PRODUCTION_DATABASE_URL
-: DEVELOPMENT_DATABASE_URL;
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
