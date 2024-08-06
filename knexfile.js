f96c656791e5f9e67ec4db17595da377538f14f8
MuluMehreteab <mulu.mehreteab@gmail.com>
2024-08-05 22:47:12 -0400

    updated env 3nd

--- knexfile.js
+++ knexfile.js
@@ -2,9 +2,11 @@ const path = require("path");
 
 require("dotenv").config();
 
-const {
+/*const {
   DATABASE_URL = "postgres://dev_bj4i_user:jecuANIFXFDoNeW7lINdkkgvo2rrVFq5@dpg-cqjvksggph6c739fcc3g-a.ohio-postgres.render.com/dev_bj4i?ssl=true",
-} = process.env;
+} = process.env;*/
+
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
