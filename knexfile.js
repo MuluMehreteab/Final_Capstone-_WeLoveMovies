3060f780cac47705e98475a0d929a06853065632
MuluMehreteab <mulu.mehreteab@gmail.com>
2024-08-07 00:23:02 -0400

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,42 +1,39 @@
-require("dotenv").config();
-
 const path = require("path");
 
-console.log(process.env.DEVELOPMENT_DATABASE_URL);
 
-const {
-  NODE_ENV = "development",
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL,
-} = process.env;
-const URL =
-  NODE_ENV === "production"
-    ? PRODUCTION_DATABASE_URL
-    : DEVELOPMENT_DATABASE_URL;
+require("dotenv").config();
+
+const { DATABASE_URL } = process.env;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: URL,
-    pool: { min: 0, max: 5 },
+    connection: DATABASE_URL,
+    pool: { min: 2, max: 10 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
+    ssl: {
+      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+    }
   },
 
   production: {
     client: "postgresql",
-    connection: URL,
-    pool: { min: 0, max: 5 },
+    connection: DATABASE_URL,
+    pool: { min: 2, max: 10 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
+    ssl: {
+      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
+    }
   },
 
   test: {
